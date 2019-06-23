0f2af576769def78c4ea605d71ce4f98dd1e0ca3
Robert D <robertrd@gmx.de>
2019-06-23 23:23:47 +0200

    finished the CameraCalibration.py: --> the distortion info is stored in the file CameraCalibration_pickle.p

--- CameraCalibration.py
+++ CameraCalibration.py
@@ -5,25 +5,36 @@ import matplotlib.image as mpimg
 import pickle
 import glob
 
+# termination criteria for corner detection in the chessboard
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
 # prepare object points of the cheassboard for camera calibration 
 nx = 9#TODO: enter the number of inside corners in x
 ny = 6#TODO: enter the number of inside corners in y
 
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((ny*nx,3),np.float32)
 objp[:,:2]=np.mgrid[0:9,0:6].T.reshape(-1,2)
 
-objpoints=[]    #3d world points
-imagespoints=[] #2d image points
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('*./camera_cal*.jpg')
+images = glob.glob('./camera_cal/*.jpg')
+#print('debug')
+#print(len(images))
+print('[CameraCalibration]...Started')
+print('')
 
-for fname, idx in images:
+for idx,fname in enumerate(images):
 # Make a list of calibration images
-	fname = 'calibration_test.png'
+	#fname = 'calibration_test.png'
 	img = cv2.imread(fname)
+    #plt.imshow(img)
 
 	# Convert to grayscale
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #imgplot = plt.imshow(img)
 
 	# Find the chessboard corners
 	ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
@@ -31,68 +42,60 @@ for fname, idx in images:
 	# If found, draw corners
 	if ret == True:
     # Draw and display the corners
-		cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
-		write_name='corners_found'+str(idx)+'.jpg'
-		vc2.write(write_name,img)
+		#print('Debug:')
+        #print('working on'+fname)
+		objpoints.append(objp)
+		corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+		imgpoints.append(corners2)
+		cv2.drawChessboardCorners(img, (nx, ny), corners2, ret)
+		write_name='Step1_CameraCalibration_AllCornersDetected'+str(idx)+'.jpg'
+		cv2.imwrite(write_name,img)
 		plt.imshow(img)
 	
+#preparation of calc the camera distortion	
+img = cv2.imread('./camera_cal/calibration2.jpg')
+img_size = (img.shape[1],img.shape[0])	
+#print('debug:')
+#print('--> Selected image: /camera_cal/calibration2.jpg: Width:' +str(img.shape[1])+', Height:'+str(img.shape[0]))
+
+#print('debug:')
+#print('objpoints: '+str(len(objpoints)))
+#print('imgpoints: '+str(len(imgpoints)))
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints,imgpoints,img_size,None,None)
 	
-	
-# Read in the saved camera matrix and distortion coefficients
-# These are the arrays you calculated using cv2.calibrateCamera()
-dist_pickle = pickle.load( open( "wide_dist_pickle.p", "rb" ) )
-mtx = dist_pickle["mtx"]
-dist = dist_pickle["dist"]
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+    mean_error += error
 
-# Read in an image
-img = cv2.imread('test_image2.png')
-nx = 8 # the number of inside corners in x
-ny = 6 # the number of inside corners in y
+reproError =mean_error/len(objpoints)
+    
+#print("--> mtx: ")
+#print(mtx)
+#print("--> dist:")
+#print(dist)
+#print("--> Reprojection error: {}".format(reproError) )    
 
+#validation of the intrinsic calibration data via reprojection error
+if (reproError>1.0):
+    print('--> Validation: Calibration is too bad. You have to repeate taking pictures!')
+if ((reproError<1.0) and (reproError>0.1)):
+    print('--> Validation: Calibration is good for use!')
+if (reproError<0.1):
+    print('--> Validation: Calibration is perfect')
 
-
-
-# Define a function that takes an image, number of x and y points, 
-# camera matrix and distortion coefficients
-def corners_unwarp(img, nx, ny, mtx, dist):
-    # Use the OpenCV undistort() function to remove distortion
-    undist = cv2.undistort(img, mtx, dist, None, mtx)
-    # Convert undistorted image to grayscale
-    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
-    # Search for corners in the grayscaled image
-    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-
-    if ret == True:
-        # If we found corners, draw them! (just for fun)
-        cv2.drawChessboardCorners(undist, (nx, ny), corners, ret)
-        # Choose offset from image corners to plot detected corners
-        # This should be chosen to present the result at the proper aspect ratio
-        # My choice of 100 pixels is not exact, but close enough for our purpose here
-        offset = 100 # offset for dst points
-        # Grab the image shape
-        img_size = (gray.shape[1], gray.shape[0])
-
-        # For source points I'm grabbing the outer four detected corners
-        src = np.float32([corners[0], corners[nx-1], corners[-1], corners[-nx]])
-        # For destination points, I'm arbitrarily choosing some points to be
-        # a nice fit for displaying our warped result 
-        # again, not exact, but close enough for our purposes
-        dst = np.float32([[offset, offset], [img_size[0]-offset, offset], 
-                                     [img_size[0]-offset, img_size[1]-offset], 
-                                     [offset, img_size[1]-offset]])
-        # Given src and dst points, calculate the perspective transform matrix
-        M = cv2.getPerspectiveTransform(src, dst)
-        # Warp the image using OpenCV warpPerspective()
-        warped = cv2.warpPerspective(undist, M, img_size)
-
-    # Return the resulting image and matrix
-    return warped, M
-
-top_down, perspective_M = corners_unwarp(img, nx, ny, mtx, dist)
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-f.tight_layout()
-ax1.imshow(img)
-ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(top_down)
-ax2.set_title('Undistorted and Warped Image', fontsize=50)
-plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+print('')      
+print('[CameraCalibration]...Done')   
+    
+# save the camera calibration coefficients for undistorting images later
+dist_pickle={}
+dist_pickle["mtx"] = mtx
+dist_pickle["dist"] = dist
+dist_pickle["repro_error"]= reproError
+#print('Debug: '
+#print(dist_pickle)
+dist_pickle = pickle.dump(dist_pickle, open( "./CameraCalibration_pickle.p", "wb" ) )
+print('[CameraCalibration]...Write calibration data to file: CameraCalibration_pickle.p [Done]')
+    
