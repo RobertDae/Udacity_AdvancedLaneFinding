0b2907e30364bd8400c1287cb3a0f762d7f6256e
Robert D <robertrd@gmx.de>
2019-06-23 23:48:26 +0200

    first implementations for undistorting the images

---
+++ ImageProcessing.py
@@ -0,0 +1,73 @@
+import numpy as np
+import cv2
+import pickle
+import glob
+
+dist_pickle = pickle.load(open('./camera_cal/CameraCalibration_pickle.p'))
+mtx =dist_pickle("mtx")
+dist=dist_pickle("dist")
+
+#undistort the testimages
+# bring them into another color scheme such as hsv
+# do some filtering for finding the lanes
+# do the bird eye view for getting the curvature of the lanes
+
+
+
+# Define a function that takes an image, number of x and y points, 
+# camera matrix and distortion coefficients
+def corners_unwarp(img, nx, ny, mtx, dist):
+    # Use the OpenCV undistort() function to remove distortion
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    # Convert undistorted image to grayscale
+    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
+    # Search for corners in the grayscaled image
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+
+    if ret == True:
+        # If we found corners, draw them! (just for fun)
+        cv2.drawChessboardCorners(undist, (nx, ny), corners, ret)
+        # Choose offset from image corners to plot detected corners
+        # This should be chosen to present the result at the proper aspect ratio
+        # My choice of 100 pixels is not exact, but close enough for our purpose here
+        offset = 100 # offset for dst points
+        # Grab the image shape
+        img_size = (gray.shape[1], gray.shape[0])
+
+        # For source points I'm grabbing the outer four detected corners
+        src = np.float32([corners[0], corners[nx-1], corners[-1], corners[-nx]])
+        # For destination points, I'm arbitrarily choosing some points to be
+        # a nice fit for displaying our warped result 
+        # again, not exact, but close enough for our purposes
+        dst = np.float32([[offset, offset], [img_size[0]-offset, offset], 
+                                     [img_size[0]-offset, img_size[1]-offset], 
+                                     [offset, img_size[1]-offset]])
+        # Given src and dst points, calculate the perspective transform matrix
+        M = cv2.getPerspectiveTransform(src, dst)
+        # Warp the image using OpenCV warpPerspective()
+        warped = cv2.warpPerspective(undist, M, img_size)
+
+    # Return the resulting image and matrix
+    return warped, M
+
+
+#make a list of unwarped images
+images = glob.glob('./test_images/test*.jpg')
+
+for idx, fname in enumerate(images):
+	#read in the images found in the test folder
+	img = cv2.imread(fname)
+	#undistort the image
+	img = cv2.undistort(img,mtx,dist,None,mtx)
+    #
+	write_name='./output/Step2_UndistortTheTestImages'+str(idx)+'.jpg'
+	cv2.imwrite(write_name,result)
+
+top_down, perspective_M = corners_unwarp(img, nx, ny, mtx, dist)
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(top_down)
+ax2.set_title('Undistorted and Warped Image', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
